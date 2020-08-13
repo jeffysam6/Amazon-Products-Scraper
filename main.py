d8350e4e5ee51be729645faa87c827edbe315689
jeffysam6 <jeffysam02@gmail.com>
2020-08-13 20:41:14 +0530

    task

---
+++ main.py
@@ -0,0 +1,102 @@
+import requests
+from bs4 import BeautifulSoup as soup
+from selenium import webdriver
+import json as JSON
+
+
+BASE_URL = 'https://www.amazon.com/s?k='
+# https://www.amazon.com/s?k=black+shoes
+
+while(True):
+	print("Enter the product  to be Scraped")
+	product_name = input()
+	if(product_name):
+		break
+	else:
+		print("Please Try Again!!")
+		print()
+
+# product_name = 'black shoes'
+
+
+option = webdriver.ChromeOptions()
+option.add_argument("--headless")
+option.add_argument("--incognito")
+
+option.add_argument("user-agent= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'")
+
+driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=option)        
+
+# driver.implicitly_wait(50)
+
+# driver.get(BASE_URL)
+
+
+
+
+# req = requests.get(BASE_URL,headers=headers)
+product_json = []
+
+missing_values = 0
+
+
+
+for page in range(1,5):
+
+	driver.get(BASE_URL+'+'.join(product_name.split())+f"&page={page}")
+
+	req = driver.page_source
+
+	page_html = soup(req,'html.parser')
+
+	product_containers = page_html.find_all('div',class_="sg-col-4-of-24 sg-col-4-of-12 sg-col-4-of-36 s-result-item s-asin sg-col-4-of-28 sg-col-4-of-16 sg-col sg-col-4-of-20 sg-col-4-of-32")
+
+	per_page_product = 0
+	rejected_per_page = 0
+
+	for products in product_containers:
+		json = {}
+		try:
+			image = products.find("img",class_='s-image')
+			stars = products.find("span",class_='a-icon-alt')
+			price = products.find('span',class_='a-offscreen')
+			json['title'] = image['alt']
+			json['image'] = image['src']
+			json['ratings'] = stars.text
+			json['price'] = price.text
+			product_json.append(json)
+			per_page_product += 1
+
+
+		except:
+			# print("Missing values")
+			missing_values += 1
+			rejected_per_page += 1
+
+	print(f"*********** Page {page} ***********************")
+	print("Products Scraped: ",per_page_product )
+	print('Products Rejected: ',rejected_per_page)
+	print()
+
+driver.close()
+
+
+with open(f"{product_name}.json", 'w', encoding='utf-8', errors='ignore') as f:
+	JSON.dump(product_json, f, ensure_ascii=False, indent=4)
+
+
+
+
+
+	# print(image['alt'],image["src"])
+
+# print(product_json)
+
+print()
+
+print("Total Products Scraped ",len(product_json))
+
+print("Total Products Rejected ",missing_values)
+
+
+
